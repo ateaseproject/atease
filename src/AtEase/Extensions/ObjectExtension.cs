b9c0dfddadac5e148cc4dbe45615fa614910e106
ahmad <achehre@yahoo.com>
2019-08-05 12:05:07 +0430

    Object extension and format extension for String  added.

---
+++ src/AtEase/Extensions/ObjectExtension.cs
@@ -0,0 +1,15 @@
+﻿namespace AtEase.Extensions
+{
+    public static class ObjectExtension
+    {
+        public static bool IsNull(this object value)
+        {
+            return value == null;
+        }
+
+        public static bool IsNotNull(this object value)
+        {
+            return value != null;
+        }
+    }
+}
--- src/AtEase/Extensions/StringExtensions.cs
+++ src/AtEase/Extensions/StringExtensions.cs
@@ -46,5 +46,20 @@
                 .Replace("8", "۸")
                 .Replace("9", "۹");
         }
+
+        public static string Format(this string value, object arg0)
+        {
+            return string.Format(value, arg0);
+        }
+
+        public static string Format(this string value, object arg0, object arg1)
+        {
+            return string.Format(value, arg0, arg1);
+        }
+
+        public static string Format(this string value, object arg0, object arg1, object arg2)
+        {
+            return string.Format(value, arg0, arg1, arg2);
+        }
     }
 }
