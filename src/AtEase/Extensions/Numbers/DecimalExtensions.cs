4c54ffa36f7925f1e095077d8a23aae83217b56a
ahmad <achehre@yahoo.com>
2019-08-25 14:21:49 +0430

    ToNullableDecimal added.

---
+++ src/AtEase/Extensions/Numbers/DecimalExtensions.cs
@@ -0,0 +1,15 @@
+﻿namespace AtEase.Extensions
+{
+    public static class DecimalExtensions
+    {
+        public static decimal? ToNullableDecimal(this double? value)
+        {
+            if (value == null)
+            {
+                return null;
+            }
+
+            return (decimal) value;
+        }
+    }
+}
---
+++ tests/AtEase.Test/DecimalExtensionsTests.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using AtEase.Extensions;
+using Xunit;
+
+namespace AtEase.Test
+{
+    public class DecimalExtensionsTests
+    {
+        public static IEnumerable<object[]> doubles =>
+            new List<object[]>
+            {
+                new object[] {null, null},
+                new object[] {(double) 1, (decimal) 1},
+                new object[] {1.1, (decimal) 1.1}
+            };
+
+
+        [Theory]
+        [MemberData(nameof(doubles))]
+        public void nullable_double_to_nullable_decimal(double? value,
+            decimal? expectedValue)
+        {
+            var decimalValue = value.ToNullableDecimal();
+            Assert.Equal(expectedValue, decimalValue);
+        }
+    }
+}
