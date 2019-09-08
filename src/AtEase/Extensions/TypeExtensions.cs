8609ccf51bdc4d35f09eab4ab7b80c38f7fb9177
ahmad <achehre@yahoo.com>
2019-09-08 15:43:08 +0430

    TypeExtensions added.

---
+++ src/AtEase/Extensions/TypeExtensions.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+
+namespace AtEase.Extensions
+{
+    public static class TypeExtensions
+    {
+        [DebuggerStepThrough]
+        public static Assembly Assembly(this Type type)
+        {
+            return type.GetTypeInfo().Assembly;
+        }
+
+        [DebuggerStepThrough]
+        public static object[] GetCustomAttributes(this Type type, Type attributeType, bool inherit)
+        {
+            return type.GetTypeInfo()
+                .GetCustomAttributes(attributeType, inherit)
+                .ToArray();
+        }
+
+        [DebuggerStepThrough]
+        public static bool IsEnum(this Type type)
+        {
+            return type.GetTypeInfo().IsEnum;
+        }
+
+        [DebuggerStepThrough]
+        public static bool IsGenericType(this Type type)
+        {
+            return type.GetTypeInfo().IsGenericType;
+        }
+
+        [DebuggerStepThrough]
+        public static bool IsInstanceOfType(this Type type, object obj)
+        {
+            return type.GetTypeInfo().IsInstanceOfType(obj);
+        }
+
+        [DebuggerStepThrough]
+        public static bool IsValueType(this Type type)
+        {
+            return type.GetTypeInfo().IsValueType;
+        }
+    }
+}
