50ff1498b977220c74a60ecd16c2345538948651
ahmad <achehre@yahoo.com>
2019-12-28 16:11:48 +0330

    feat: add random decimal

---
+++ src/AtEase/Extensions/RandomExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace AtEase.Extensions
+{
+    public static class RandomExtensions
+    {
+        [DebuggerStepThrough]
+        public static int NextInt32(this Random rng)
+        {
+            var firstBits = rng.Next(0, 1 << 4) << 28;
+            var lastBits = rng.Next(0, 1 << 28);
+            return firstBits | lastBits;
+        }
+
+        [DebuggerStepThrough]
+        public static decimal NextDecimal(this Random rng)
+        {
+            var scale = (byte) rng.Next(29);
+            var sign = rng.Next(2) == 1;
+            return new decimal(rng.NextInt32(),
+                rng.NextInt32(),
+                rng.NextInt32(),
+                sign,
+                scale);
+        }
+    }
+}
