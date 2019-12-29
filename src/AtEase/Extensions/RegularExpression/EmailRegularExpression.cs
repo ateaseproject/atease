f3f54ca691bb1f5a40982f9316b334e108ddb3f8
ahmad <achehre@yahoo.com>
2019-12-29 14:41:49 +0330

    feat: add email regular expression

---
+++ src/AtEase/Extensions/RegularExpression/EmailRegularExpression.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Globalization;
+using System.Text.RegularExpressions;
+
+namespace AtEase.Extensions.RegularExpression
+{
+    public static class EmailRegularExpression
+    {
+        // from https://docs.microsoft.com/en-us/dotnet/standard/base-types/how-to-verify-that-strings-are-in-valid-email-format
+
+        public const string EmailRegex =
+            @"^(?("")("".+?(?<!\\)""@)|(([0-9a-z]((\.(?!\.))|[-!#\$%&'\*\+/=\?\^`\{\}\|~\w])*)(?<=[0-9a-z])@))" +
+            @"(?(\[)(\[(\d{1,3}\.){3}\d{1,3}\])|(([0-9a-z][-0-9a-z]*[0-9a-z]*\.)+[a-z0-9][\-a-z0-9]{0,22}[a-z0-9]))$";
+
+        public const string EmailComaSeparatedRegex = "";
+
+
+        public static bool IsValidEmail(this string email, TimeSpan? matchTimeout = null)
+        {
+            if (email.IsNullOrEmptyOrWhiteSpace())
+            {
+                return false;
+            }
+
+            try
+            {
+                // Normalize the domain
+                email = Regex.Replace(email, @"(@)(.+)$", DomainMapper,
+                    RegexOptions.None, TimeSpan.FromMilliseconds(200));
+
+                // Examines the domain part of the email and normalizes it.
+                string DomainMapper(Match match)
+                {
+                    // Use IdnMapping class to convert Unicode domain names.
+                    var idn = new IdnMapping();
+
+                    // Pull out and process domain name (throws ArgumentException on invalid)
+                    var domainName = idn.GetAscii(match.Groups[2]
+                        .Value);
+
+                    return match.Groups[1]
+                               .Value + domainName;
+                }
+            }
+            catch (RegexMatchTimeoutException e)
+            {
+                return false;
+            }
+            catch (ArgumentException e)
+            {
+                return false;
+            }
+
+            try
+            {
+                return Regex.IsMatch(email, EmailRegex,
+                    RegexOptions.IgnoreCase, TimeSpan.FromMilliseconds(250));
+            }
+            catch (RegexMatchTimeoutException)
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ tests/AtEase.Test/RegularExpressionEmailIsEmailTests.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using AtEase.Extensions.RegularExpression;
+using Xunit;
+
+namespace AtEase.Test
+{
+    public class RegularExpressionEmailIsEmailTests
+    {
+        public static IEnumerable<object[]> Email =>
+            new List<object[]>
+            {
+                new object[] {" ", false},
+                new object[] {"david.jones@proseware.com", true},
+                new object[] {"d.j@server1.proseware.com", true},
+                new object[] {"jones@ms1.proseware.com", true},
+                new object[] {"j.@server1.proseware.com", false},
+                new object[] {"j@proseware.com9", true},
+                new object[] {"js#internal@proseware.com", true},
+                new object[] {"j_9@[129.126.118.1]", true},
+                new object[] {"j..s@proseware.com", false},
+                new object[] {"js*@proseware.com", false},
+                new object[] {"js@proseware..com", false},
+                new object[] {"js@proseware.com9", true},
+                new object[] {"j.s@server1.proseware.com", true},
+                new object[] {"\"j\\\"s\\\"\"@proseware.com", true},
+                new object[] {"js@proseware.ایران", true},
+                new object[] {"js@contoso.中国", true},
+            };
+
+
+        [Theory]
+        [MemberData(nameof(Email))]
+        public void enum_to_int(string email, bool isValid)
+        {
+            Assert.Equal(isValid, email.IsValidEmail());
+        }
+    }
+}
