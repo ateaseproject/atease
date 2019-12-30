21835f91fa068484ebf0f4452c451accb76c2cce
ahmad <achehre@yahoo.com>
2019-12-30 10:51:16 +0330

    fix: add ShamsiYear to DisplayNameResource

--- src/AtEase/Resources/DisplayNameResource.Designer.cs
+++ src/AtEase/Resources/DisplayNameResource.Designer.cs
@@ -19,7 +19,7 @@ namespace AtEase.Resources {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     public class DisplayNameResource {
@@ -273,6 +273,15 @@ namespace AtEase.Resources {
         public static string PhoneNumber {
             get {
                 return ResourceManager.GetString("PhoneNumber", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to سال شمسی.
+        /// </summary>
+        public static string ShamsiYear {
+            get {
+                return ResourceManager.GetString("ShamsiYear", resourceCulture);
             }
         }
         
