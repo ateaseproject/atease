34639e10a31c495b693e6c50343649b214f176e5
ahmad <achehre@yahoo.com>
2022-03-01 01:34:04 +0330

    Get attribute field value

--- src/AtEase/Extensions/ReflectionExtensions.cs
+++ src/AtEase/Extensions/ReflectionExtensions.cs
@@ -1,10 +1,14 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Linq;
 using System.Reflection;
 
 namespace AtEase.Extensions
 {
     public static class ReflectionExtensions
     {
+        
+        [DebuggerStepThrough]
         public static bool HasAttribute<T>(this Type type) where T : Attribute
         {
             if (type == null)
@@ -13,6 +17,37 @@ namespace AtEase.Extensions
             }
 
             return type.GetCustomAttribute<T>() != null;
+        }
+
+        /// <summary>
+        ///     Get property value of object by property name.
+        /// </summary>
+        [DebuggerStepThrough]
+        public static object GetPropertyValue(this object src, string propertyName)
+        {
+            return src.GetType().GetProperty(propertyName)?.GetValue(src, null);
+        }
+
+        /// <summary>
+        ///     Get field value of object by field name.
+        /// </summary>
+        [DebuggerStepThrough]
+        public static object GetFieldValue(this object src, string fieldName)
+        {
+            return src.GetType().GetField(fieldName)?.GetValue(src);
+        }
+
+        /// <summary>
+        ///     Get single attribute field value. Note that attribute should be single.
+        /// </summary>
+        [DebuggerStepThrough]
+        public static string SingleAttributeValue<TAttribute>(this Type type, string propertyName, string attributeName)
+            where TAttribute : Attribute
+        {
+            PropertyInfo[] props = type.GetProperties();
+            var prop = props.Single(p => p.Name == propertyName);
+            object attr = prop.GetCustomAttributes(true).Single(a => a.GetType() == typeof(TAttribute));
+            return attr.GetFieldValue(attributeName).ToString();
         }
     }
 }
---
+++ tests/AtEase.Test/ReflectionExtensionsTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using AtEase.Extensions;
+using FluentAssertions;
+using Xunit;
+
+namespace AtEase.Test
+{
+    [AttributeUsage(AttributeTargets.Property |
+                    AttributeTargets.Struct)
+    ]
+    public class CustomAttrAttribute : Attribute
+    {
+        public string Name;
+
+        public CustomAttrAttribute(string name)
+        {
+            Name = name;
+        }
+    }
+
+    public class CustomClass
+    {
+        [CustomAttr("SomeName")]
+        public int SomeProp { get; set; }
+    }
+
+    public class ReflectionExtensionsTests
+    {
+        [Fact]
+        public void SingleAttributeValue_should_return_attribute_value()
+        {
+            CustomClass customClass = new CustomClass();
+
+            string attributeValue = customClass.GetType().SingleAttributeValue<CustomAttrAttribute>("SomeProp", "Name");
+
+            attributeValue.Should().Be("SomeName");
+        }
+    }
+}
