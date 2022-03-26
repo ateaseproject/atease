5684958920fde292c15aa6c1473a3973abe33c85
ahmad <achehre@yahoo.com>
2022-03-26 11:58:09 +0430

    check min value for decimal

--- src/AtEase/Extensions/Numbers/DecimalExtensions.cs
+++ src/AtEase/Extensions/Numbers/DecimalExtensions.cs
@@ -9,7 +9,17 @@
         /// <returns>Nullable decimal</returns>
         public static decimal? ToNullableDecimal(this double? value)
         {
-            return (decimal?) value;
+            return (decimal?)value;
         }
+        /// <summary>
+        /// Indicates the value as decimal is 0 or decimal.MinValue value.
+        /// </summary>
+        /// <param name="value">decimal value</param>
+        /// <returns>Returns true if value is 0 or min value.</returns>
+        public static bool IsZeroOrMin(this decimal value)
+        {
+            return value is decimal.MinValue or 0;
+        }
+
     }
 }
