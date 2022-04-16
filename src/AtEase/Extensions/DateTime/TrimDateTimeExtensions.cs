433d186cfb975f5f6bf3abceb824ea545b95ebbf
ahmad <achehre@yahoo.com>
2022-04-16 16:53:31 +0430

    trim date time

---
+++ src/AtEase/Extensions/DateTime/TrimDateTimeExtensions.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace AtEase.Extensions
+{
+    public static class TrimDateTimeExtensions
+    {
+        private static DateTime Trim(this DateTime date, long roundTicks)
+        {
+            return new DateTime(date.Ticks - date.Ticks % roundTicks, date.Kind);
+        }
+
+        /// <summary>
+        ///     Trim time of the DateTime
+        /// </summary>
+        public static DateTime TrimUntilDays(this DateTime value)
+        {
+            return value.Trim(TimeSpan.TicksPerDay);
+        }
+
+        /// <summary>
+        ///     Trim minute, seconds and milliseconds of the DateTime
+        /// </summary>
+        public static DateTime TrimUntilHours(this DateTime value)
+        {
+            return value.Trim(TimeSpan.TicksPerHour);
+        }
+
+        /// <summary>
+        ///     Trim  seconds and milliseconds of the DateTime
+        /// </summary>
+        public static DateTime TrimUntilMinutes(this DateTime value)
+        {
+            return value.Trim(TimeSpan.TicksPerMinute);
+        }
+
+        /// <summary>
+        ///     Trim  milliseconds of the DateTime
+        /// </summary>
+        public static DateTime TrimUntilSeconds(this DateTime value)
+        {
+            return value.Trim(TimeSpan.TicksPerSecond);
+        }
+    }
+}
---
+++ tests/AtEase.Test/TrimDateTimeExtensionsTests.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using AtEase.Extensions;
+using FluentAssertions;
+using Xunit;
+
+namespace AtEase.Test
+{
+    public class TrimDateTimeExtensionsTests
+    {
+        [Fact]
+        public void TrimUntilSeconds_should_trim_the_milliseconds()
+        {
+            DateTime dateTime = DateTime.Now;
+
+            DateTime expectedTime = new DateTime(dateTime.Year,
+                                                 dateTime.Month,
+                                                 dateTime.Day,
+                                                 dateTime.Hour,
+                                                 dateTime.Minute,
+                                                 dateTime.Second,
+                                                 0);
+
+            DateTime actualTime = dateTime.TrimUntilSeconds();
+
+            actualTime.Should().Be(expectedTime);
+        }
+
+
+        [Fact]
+        public void TrimUntilMinutes_should_trim_the_seconds()
+        {
+            DateTime dateTime = DateTime.Now;
+
+            DateTime expectedTime = new DateTime(dateTime.Year,
+                                                 dateTime.Month,
+                                                 dateTime.Day,
+                                                 dateTime.Hour,
+                                                 dateTime.Minute,
+                                                 0,
+                                                 0);
+
+            DateTime actualTime = dateTime.TrimUntilMinutes();
+
+            actualTime.Should().Be(expectedTime);
+        }
+
+        [Fact]
+        public void TrimUntilHours_should_trim_the_minutes()
+        {
+            DateTime dateTime = DateTime.Now;
+
+            DateTime expectedTime = new DateTime(dateTime.Year, dateTime.Month, dateTime.Day, dateTime.Hour, 0, 0, 0);
+
+            DateTime actualTime = dateTime.TrimUntilHours();
+
+            actualTime.Should().Be(expectedTime);
+        }
+
+        [Fact]
+        public void TrimUntilDays_should_trim_the_hourse()
+        {
+            DateTime dateTime = DateTime.Now;
+
+            DateTime expectedTime = new DateTime(dateTime.Year, dateTime.Month, dateTime.Day, 0, 0, 0, 0);
+
+            DateTime actualTime = dateTime.TrimUntilDays();
+
+            actualTime.Should().Be(expectedTime);
+        }
+    }
+}
